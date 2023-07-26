653d2cd0f81aa7a0414145c4302337554366af92
Raki <marianaraquelvegas@gmail.com>
2023-07-25 21:12:12 -0300

    Se agregan modelos

--- control_cursos/models.py
+++ control_cursos/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+#modelo de curso
+
+class Curso(models.Model):
+    
+    nombre = models.CharField(max_length=64)
+    comision = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre}, {self.comision}"
+    
+#modelo de estudiante
+class Estudiante(models.Model):
+    apellido = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    email = models.EmailField(blank=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    dni = models.CharField(max_length=32)
+    fecha_nacimiento = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
+    
+#modelo de profesor
+class Profesor(models.Model):
+    apellido = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    dni = models.CharField(max_length=32)
+    email = models.EmailField(blank=True)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    profesion = models.CharField(max_length=128)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
+
+
+#modelo de entregas
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=256)
+    fecha_entrega = models.DateTimeField(auto_now_add=True)
+    esta_aprobado = models.BooleanField(default=False)  
+           
